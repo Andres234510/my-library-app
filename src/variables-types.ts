be094fb3c9ca8414e7412be71d11df5a7ca1cfe1
Andres Felipe Gutierrez Vera <andres2345v@gmail.com>
2024-06-13 08:31:34 -0500

    tipado primitivo

---
+++ src/variables-types.ts
@@ -0,0 +1,29 @@
+// Declaración de una variable de tipo string
+let bookTitle: string = "TypeScript Basics";
+
+// Declaración de una constante de tipo string
+const libraryName: string = "My Library";
+
+// Declaración de una variable de tipo number con un valor numérico (por ejemplo, 42)
+let numberOfBooks: number = 69;
+
+// Declaración de una variable de tipo boolean con el valor true
+let isLibraryOpen: boolean = true;
+
+// Declaración de variables para un libro
+let titulo: string = "Harry Potter y la piedra filosofal"; 
+let autor: string = "J.K. Rowling"; 
+let genero: string = "fantasia";
+let idioma: string = "Castellano";
+let precio: number = 47000;
+let formato: string = "Tapa blanda bolsillo";
+let isbn: string = "9788498382662"; 
+let descripcion: string =  "Con las manos temblorosas, Harry le dio la vuelta al sobre y vio un  sello de lacre púrpura con un escudo de armas: un león, un águila, un tejón y una serpiente, que rodeaban una gran letra H. Harry Potter nunca ha oído hablar de Hogwarts hasta que empiezan a caer  cartas en el felpudo del número 4 de Privet Drive. Llevan la dirección  escrita con tinta verde en un sobre de pergamino amarillento con un  sello de lacre púrpura, y sus horripilantes tíos se apresuran a  confiscarlas. Más tarde, el día que Harry cumple once años, Rubeus  Hagrid, un hombre gigantesco cuyos ojos brillan como escarabajos negros,  irrumpe con una noticia extraordinaria: Harry Potter es un mago, y le  han concedido una plaza en el Colegio Hogwarts de Magia y Hechicería. ¡Está a punto de comenzar una aventura increíble!";
+let estado: string = "Nuevo"; 
+let ubicacion: string = "la casa del libro";
+let fecha_publicacion: string = '1997-07-26'; 
+let editorial: string = "Salamandra Infantil y Juvenil"; 
+let paginas: number = 264; 
+let dimensiones: string = "ancho : 13.7cm X alto : 22cm X grueso : 2.2cm";
+let peso: number = 397; 
+let disponible: boolean = true;
